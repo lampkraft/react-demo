25754c8d289d4065e78bc8e65f2f5e4f5114e67e
lampkraft <lampkraft@gmail.com>
2019-09-23 20:54:09 +0200

    Search view component

---
+++ src/components/search/search-view.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { ISearchViewProps } from './search.types';
+import './search-view.css';
+import Spinner from 'react-bootstrap/Spinner'
+import Button from 'react-bootstrap/Button';
+
+library.add(faSearch);
+library.add(faTimes);
+
+export function SearchView(props: ISearchViewProps) {
+
+  let searchValue: string = '';
+
+  const onSearch = (event: any) => {
+    // emit search
+    console.log('searching ', event);
+    searchValue = event.target.value;
+    props.search(searchValue);
+  };
+
+  const onSelect = (eventKey: any, _event: Object) => {
+    props.select(parseInt(eventKey, 10));
+  }
+
+  const onClear = () => {
+    props.clear();
+  }
+
+  return (
+    <div className="search-wrapper">
+      <InputGroup className="mb-3">
+        <InputGroup.Prepend>
+          <InputGroup.Text>
+            {props.loading ? <Spinner animation="border" size="sm" /> : <FontAwesomeIcon icon="search" />}
+          </InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl
+          placeholder={props.placeholder}
+          onChange={onSearch}
+          value={props.inputValue}
+          aria-label={props.placeholder}
+        />
+        <InputGroup.Append>
+          <Button variant="outline-secondary" onClick={onClear}><FontAwesomeIcon icon="times" /></Button>
+        </InputGroup.Append>
+        <div className="prevent-blur autocomplete">
+          <Dropdown show={props.showDropdown}>
+            <Dropdown.Menu>
+              {props.autoCompleteItems
+                .map(item =>
+                  <Dropdown.Item
+                    onSelect={onSelect}
+                    key={item.id}
+                    eventKey={item.id as any}>
+                    {item.namn}
+                  </Dropdown.Item>
+                )}
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </InputGroup>
+    </div>
+  );
+}

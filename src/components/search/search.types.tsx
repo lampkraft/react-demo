901be5b43572459baded0a6616dc5bbf4beb0f25
lampkraft <lampkraft@gmail.com>
2019-09-23 20:52:29 +0200

    Types for search related properties

---
+++ src/components/search/search.types.tsx
@@ -0,0 +1,29 @@
+import { FocusEvent } from "react";
+
+export interface ISearchViewProps {
+	search: Function;
+	select: Function;
+	showDropdown: boolean;
+	autoCompleteItems: IFoodAutoComplete[];
+	placeholder: string;
+	loading: boolean;
+	inputValue: string;
+	clear: Function;
+}
+
+export interface IFoodAutoComplete {
+	id: number;
+	namn: string;
+	arLivsmedel: boolean;
+	bildtyp: number;
+}
+
+export interface ISearchContainerState {
+	autoCompleteItems: IFoodAutoComplete[],
+	selectedFoodItemId: string | null,
+	result: any | null,
+	showDropdown: boolean;
+	loading: boolean;
+	inputValue: string;
+	errorMsg: string;
+}
